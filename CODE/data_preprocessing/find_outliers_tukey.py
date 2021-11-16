fb512f766f1217fafb80dd221692867e3a053fcb
melodykaagman <melodykaagman@hotmail.com>
2021-11-16 20:13:15 +0100

    making data preprocessing folder

---
+++ CODE/data_preprocessing/find_outliers_tukey.py
@@ -0,0 +1,18 @@
+def find_outliers_tukey(x):
+    """
+    Outlier detection
+    Input:
+        - 
+    Output:
+        - 
+    """
+    import numpy as np
+    q1 = np.percentile(x,25)
+    q3 = np.percentile(x,75)
+    iqr = q3-q1
+    floor = q1- 1.5 * iqr
+    ceiling = q3 + 1.5 * iqr
+    outlier_indices = list(x.index[(x< floor)|(x> ceiling)])
+    outlier_values = list(x[outlier_indices])
+    
+    return outlier_indices, outlier_values
