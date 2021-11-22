c6f7da91b8da98fd451c8ff102e0a85c050ee4a3
regina <reginanockerts@gmail.com>
2021-11-22 19:23:40 +0100

    updated 'main'

--- main.py
+++ main.py
@@ -1,12 +1,65 @@
-# from CODE.features import venue_citations
-# from CODE.features import length_title
-from CODE.features.venue_frequency import venue_frequency 
-from CODE.features.venue_citations import venues_citations
+"""
+SETUP
+"""
+### Import models
+import pandas as pd
+import numpy as np
+
+### Import self-made functions
+from CODE.data_preprocessing.split_val import split_val
 from CODE.features.length_title import length_title
+from CODE.features.field_variety import field_variety2
+#from CODE.features.field_variety import field_variety
+from CODE.features.team_size import team_size
+from CODE.features.topic_variety import topics_variety
+from CODE.features.venue_frequency import venue_frequency
 
-# print(venue_frequency('DATA/train-1.json'))
+### Get the full train set:
+data = pd.read_json('DATA/train-1.json')   # Numerical columns: 'year', 'references', 'citations'
 
-# print(venues_citations('DATA/train-1.json'))
+### push the numerical columns to X and outcome to y
+end = len(data)
+num_X = data.loc[ 0:end+1 , ('doi', 'citations', 'year', 'references') ]
 
-print(length_title('DATA/train-1.json'))
 
+"""
+FEATURE DATAFRAME: num_X
+
+ALL: After writing a funtion to create a feature, please incorporate your new feature as a column on the dataframe below.
+This is the dataframe we will use to train the models.
+"""
+
+### use feature function to create a new variable
+title_len = length_title(data)  # returns: dictionary of lists: [doi](count)
+field_var = field_variety2(data)  # returns: dictionary of lists: [doi](count)
+team_sz = team_size(data) # returns a numbered series
+topic_var = topics_variety(data) # returns a numbered series
+venue_freq = venue_frequency(data) # returns a dictionary: [venue](count)
+
+### join the variables (type = series) to num_X 
+num_X['team_size'] = team_sz
+num_X['topic_variety'] = topic_var
+
+### join the variables (type = dictionary) to num_X
+num_X['title_length'] = num_X['doi'].map(title_len)
+num_X['field_variety'] = num_X['doi'].map(field_var)
+
+
+
+"""
+INSERT trainv/val split here
+"""
+
+
+
+
+"""
+INSERT split X and y on the train here
+"""
+
+
+
+
+"""
+IMPLEMENT model fuctions here
+"""
