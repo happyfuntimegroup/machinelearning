2f75cc2ecc2a19921044955e8b64df9b0e915f1a
regina <reginanockerts@gmail.com>
2021-11-23 14:46:11 +0100

    odds and ends

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,12 +1,89 @@
-# from CODE.features import venue_citations
-# from CODE.features import length_title
-from CODE.features.venue_frequency import venue_frequency 
-from CODE.features.venue_citations import venues_citations
+"""
+SETUP
+"""
+### Import models
+import pandas as pd
+import numpy as np
+
+### Import self-made functions
+from CODE.data_preprocessing.split_val import split_val
 from CODE.features.length_title import length_title
+from CODE.features.field_variety import field_variety2
+#from CODE.features.field_variety import field_variety
+from CODE.features.team_size import team_size
+from CODE.features.topic_variety import topics_variety
+from CODE.features.venue_frequency import venue_frequency
+from CODE.features.age import age
 
-# print(venue_frequency('DATA/train-1.json'))
+### Get the full train set:
+data = pd.read_json('DATA/train-1.json')   # Numerical columns: 'year', 'references', 'citations'
+test = pd.read_json('DATA/test.json')
 
-# print(venues_citations('DATA/train-1.json'))
 
-print(length_title('DATA/train-1.json'))
+"""
+DEAL with missing values in "data" and "test" here - SELIN
 
+doi --> ""
+title --> ""
+abstract --> "" 
+authors --> [""]
+venue --> ""
+year --> mean of venue based on "data" ELSE from "data"
+references --> 0  --think about this!
+topic --> [""]
+is_open-access --> base on venue ELSE from "data"
+fields_of_study --> [""]
+citations --> assume not blank
+
+"""
+
+
+### push the numerical columns to num_X
+end = len(data)
+num_X = data.loc[ 0:end+1 , ('doi', 'citations', 'year', 'references') ]  ##REMOVE DOI
+
+
+
+
+"""
+FEATURE DATAFRAME: num_X
+
+ALL: After writing a funtion to create a feature, please incorporate your new feature as a column on the dataframe below.
+This is the dataframe we will use to train the models.
+"""
+
+### use feature function to create a new variable
+title_len = length_title(data)      # returns: dictionary of lists: [doi](count)
+field_var = field_variety2(data)    # returns: dictionary of lists: [doi](count)
+team_sz = team_size(data)           # returns a numbered series
+topic_var = topics_variety(data)    # returns a numbered series
+venue_freq = venue_frequency(data)  # returns a dictionary: [venue](count)
+paper_age = age(data)                     # returns a numbered series
+
+### join the variables (type = series) to num_X 
+num_X['team_size'] = team_sz
+num_X['topic_variety'] = topic_var
+num_X['age'] = paper_age
+
+### join the variables (type = dictionary) to num_X
+num_X['title_length'] = num_X['doi'].map(title_len)
+num_X['field_variety'] = num_X['doi'].map(field_var)
+
+### train/val split
+X_train, X_val, y_train, y_val = split_val(num_X, target_variable = 'citations')
+
+
+
+
+
+"""
+INSERT outlier detection on X_train here - ALBERT
+"""
+
+
+
+
+"""
+IMPLEMENT regression models fuctions here
+- exponential
+"""
