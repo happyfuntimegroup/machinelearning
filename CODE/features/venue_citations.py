e66ca3d44c754501571e3588e24be4d700be2545
Albert de Vries <albertwdevries@gmail.com>
2021-11-28 15:02:18 +0100

    Now functions with pandas and returns the VenPresL score for the entire dataset (pandas Series)

--- CODE/features/venue_citations.py
+++ CODE/features/venue_citations.py
@@ -1,36 +1,37 @@
 import pandas as pd
 
-def venues_citations(source_file):
+def venues_citations(data, venue_db):
     """Output: 
     {Venue : [list of citations per paper]
     """
-    papers = pd.read_json(source_file)
     venues = {}
     citations = []
-
-    for i in range(len(papers)):
-        venue_temp = papers.iloc[i]['venue']
-        venue = ''
-
-        for char in venue_temp:
-            if char.isupper():
-                venue += char
-
-            if venue in venues.keys():
-                citations = venues[venue]
-                citations.append(papers.iloc[i]['citations'])
-                venues[venue] = citations
-            else:
-                citations.append(papers.iloc[i]['citations'])
-                venues[venue] = [(papers.iloc[i]['citations'])]
+    venue_venPresL = {}
+    
+    out = pd.Series(dtype=pd.Float64Dtype())
+    
+    for index, i_paper in data.iterrows():
+        venue = i_paper['venue']
+    
+        if venue in venues.keys():
+            citations = venues[venue]
+            citations.append(i_paper['citations'])
+            venues[venue] = citations
+        else:
+            citations.append(i_paper['citations'])
+            venues[venue] = [(i_paper['citations'])]
 
 
-        for key, value in venues.items():
-            print(key, ':', value)
-            sum_venue = sum(value)
-            print(key, ':', sum_venue)
-            venPresL = (sum_venue / len(value))
-            print(venPresL)
-            
-
-
+    for key, value in venues.items():
+        sum_venue = sum(value)
+        venPresL = (sum_venue / len(value))      
+        venue_venPresL[key] = venPresL
+    
+    # For each paper in the dataset, add the venPresL score to that paper; i.e. look at the venue in which it was published,
+    #   and add the score to that index in the pandas Series.
+    for index, i_paper in data.iterrows():
+        venue = i_paper['venue']
+        out[index,] = venue_venPresL[venue]
+    
+    # Output
+    return out # Pandas Series of VenPresL for each paper
