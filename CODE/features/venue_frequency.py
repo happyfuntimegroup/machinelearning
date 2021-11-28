e615476dee5c88d524d57c7e2047e651fe13062d
Albert de Vries <albertwdevries@gmail.com>
2021-11-28 10:20:56 +0100

    Minor output changes

--- CODE/features/venue_frequency.py
+++ CODE/features/venue_frequency.py
@@ -4,18 +4,21 @@ def venue_frequency(data):
     papers = data
     venues_count = {}
     count = 1
-
-    for i in range(len(papers)):
-        venue_temp = papers.iloc[i]['venue']
+    
+    venue_reformatted = pd.Series()
+    
+    for index, i_paper in data.iterrows():
+        venue_temp = papers.iloc[index]['venue']
         venue = ''
 
         for char in venue_temp:
             if char.isupper():
                 venue += char
             
-            if venue in venues_count.keys():
-                venues_count[venue] += 1
-            else:
-                venues_count[venue] = 1
+        if venue in venues_count.keys():
+            venues_count[venue] += 1
+        else:
+            venues_count[venue] = 1
     
-    return venues_count
+        venue_reformatted[index,] = venue
+    return venues_count, venue_reformatted
