538e3853d26054a6deced7f409c6c150d2cf9d96
regina <reginanockerts@gmail.com>
2021-11-28 15:55:47 +0100

    can set top and bottom thresholds

--- CODE/data_preprocessing/find_outliers_tukey.py
+++ CODE/data_preprocessing/find_outliers_tukey.py
@@ -1,14 +1,14 @@
-def find_outliers_tukey(x):
+def find_outliers_tukey(x, top = 75, bottom = 25):
     """
     Outlier detection
-    Input:
+    Input: column of a df, optional top and bottom thresholds as percentiles
         - 
-    Output:
+    Output: two lists, one of indices, one of values
         - 
     """
     import numpy as np
-    q1 = np.percentile(x,25)
-    q3 = np.percentile(x,75)
+    q1 = np.percentile(x, bottom)
+    q3 = np.percentile(x, top)
     iqr = q3-q1
     floor = q1- 1.5 * iqr
     ceiling = q3 + 1.5 * iqr
