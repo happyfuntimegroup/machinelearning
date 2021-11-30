e385faf19882c8c001c6010b4e15ac4f719660c1
Albert de Vries <albertwdevries@gmail.com>
2021-11-30 21:19:48 +0100

    Added H-index functions

---
+++ CODE/features/author_h_index.py
@@ -0,0 +1,17 @@
+def author_h_index(data, author_citation_dic):
+    """
+    
+    """
+    def h_check(val):
+        """
+        
+        """
+        n = len(val)
+        for h in range(n,-1,-1):
+            list_binary = [1 if citation >= h else 0 for citation in val]
+            if sum(list_binary) >= h:   # Should change this condition, cause it returns 0 for a list of [18, 0, 16]
+                return h        
+    h_index = {}
+    for key, val in author_citation_dic.items():
+        h_index[key] = h_check(val)
+    return(h_index)
