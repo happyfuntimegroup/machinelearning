39b15243fddd835fb1467e78d35e9a2a23a7eab6
Albert de Vries <albertwdevries@gmail.com>
2021-11-30 21:35:14 +0100

    Woops, forgot the version that was actually functioning

--- CODE/features/paper_h_index.py
+++ CODE/features/paper_h_index.py
@@ -3,6 +3,7 @@ def paper_h_index(data, author_citation_dic):
     
     """
     import pandas as pd
+    import math
     from CODE.features.author_h_index import author_h_index
     
     out = pd.Series(dtype=pd.Float64Dtype())
@@ -12,6 +13,9 @@ def paper_h_index(data, author_citation_dic):
         authors = i_paper['authors']
         for i_author in authors:
             h_team[i_author] = h_index[i_author]
-        max_h = max(h_team.values())
+        if len(h_team) != 0:
+            max_h = max(h_team.values())
+        else:
+            max_h = math.nan
         out.loc[index] = max_h
     return(out)
