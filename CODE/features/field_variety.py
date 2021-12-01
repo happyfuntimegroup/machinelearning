447e0e5eecc032debd0e68f4eda72162dc232d17
melodykaagman <melodykaagman@hotmail.com>
2021-12-01 17:50:19 +0100

    starting with field popularity

--- CODE/features/field_variety.py
+++ CODE/features/field_variety.py
@@ -13,33 +13,8 @@ def field_variety(data):
     """
     
     Field_variety = pd.Series([len(i) for i in data['fields_of_study']])      # Variety of fields
-
-    field_popularity_dict = {}
-    fields_dict = {}
-
-
-    for index, i_paper in data.iterrows():
-        citations = []
-
-        fields = i_paper['fields_of_study']
-        if fields == None:
-            fields = ['Missing']
-
-        citation = i_paper['citations']
-
-        for field in fields:
-            if field in field_popularity_dict.keys():
-                field_popularity_dict[field] += 1
-            else:
-                field_popularity_dict[field] = 1
-            
-            if field in fields_dict.keys():
-                citations.append(citation)
-                fields_dict[field] = sum(citations) / len(citations)
-            else:
-                fields_dict[field] = citation
-
-        return Field_variety, field_popularity_dict
+    
+    return Field_variety
         
 
 
