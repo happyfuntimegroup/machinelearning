c9151c8c6e8cc8669cde040b0bf2d2bde7f4ed2d
melodykaagman <melodykaagman@hotmail.com>
2021-12-02 12:34:05 +0100

    new features topic and field

--- CODE/features/field_citations_avarage.py
+++ CODE/features/field_citations_avarage.py
@@ -1,5 +1,38 @@
+import pandas as pd
+
+def field_citations_avarage(data):
+    """
+    Computes the avarage citations for each topic
+    Input:
+        - df['fields_of_study']:    dataframe (dataset); 'fields_of_study' column                   [pandas dataframe]
+    Output:
+        - Avarage citations:                                                            [int]
+    """
+    citations = []
+    fields_dict = {}
+    
+    out = pd.Series(dtype=pd.Float64Dtype())
+
+    for index, i_paper in data.iterrows():
+
+        fields = i_paper['fields_of_study']
+        citation = i_paper['citations']
+
+        for field in fields:
             if field in fields_dict.keys():
                 citations.append(citation)
-                fields_dict[field] = sum(citations) / len(citations)
+                fields_dict[field] = citations
             else:
                 fields_dict[field] = citation
+
+    for index, i_paper in data.iterrows():
+        fields = i_paper['fields_of_study']
+        all_the_citations = []
+        for field in fields:
+            if field in fields_dict.keys():
+                all_the_citations.append(fields_dict[field])
+        
+        avarage = sum(all_the_citations) / len(all_the_citations)
+        out[index,] = avarage 
+    
+    return out 
