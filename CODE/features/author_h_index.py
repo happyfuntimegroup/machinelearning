641fc8a6faf000045e53e6635d01ea27c6d626fe
Albert de Vries <albertwdevries@gmail.com>
2021-12-05 10:23:39 +0100

    Added comments

--- CODE/features/author_h_index.py
+++ CODE/features/author_h_index.py
@@ -1,10 +1,28 @@
 def author_h_index(author_citation_dic):
     """
-    
+    Compute the h-index for each author.
+    The h-index is defined as the value for which the author has h published papers with atleast h number of citations.
+    Input:
+        - author_citation_dic:  dataset (dict) of all authors (keys) in the known dataset 
+                                 and a list with citations of papers they worked on (values)    [dict]
+    Output:
+        - h_index:              dataset (dict) of all authors (keys) in the known dataset
+                                 and the h-index for that author (value)                        [dict]
     """
     def h_check(val):
         """
-        
+        Take a list with all the citations of a paper that one specific author worked on.
+        Then binarizes that list it for a certain h-value. Stop until the condition is met where h-value = sum of binary list.
+            i.e.:   - for a list of n published papers, take h-value = n
+                    - binarize the list for that h-value: look for papers the citation # is equal to or greater than the h-value
+                    - take the sum of the binarized list
+                    - is the sum of the binarized list equal or greater than the h-value?
+                        - if yes, then h = n
+                        - if not, take h = h-1 and redo the steps until either h = a number or h = 0
+        Input:
+            val:    list of all citations for one author
+        Output:
+            h:      h-index for that author
         """
         n = len(val)
         for h in range(n,-1,-1):
