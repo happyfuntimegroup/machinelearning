19276b4db4fe6aa97144c044a51b49218bccfd82
regina <reginanockerts@gmail.com>
2021-12-07 18:46:19 +0100

    odd

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ num_X = num_X[num_X['topic_var'] < 60]
 num_X = num_X[num_X['venPresL'] < 300]
 num_X = num_X[num_X['h_index'] < 30]
 
-%store num_X
+# %store num_X
 ##########################################
 #            Train/val split             #
 ##########################################
