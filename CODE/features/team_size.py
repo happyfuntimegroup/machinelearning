61f3eaaf1120aa6c2d209bbc24d13c394918ed85
Albert de Vries <albertwdevries@gmail.com>
2021-12-08 14:49:49 +0100

    Cleaned up

--- CODE/features/team_size.py
+++ CODE/features/team_size.py
@@ -1,16 +1,14 @@
+import pandas as pd
 def team_size(df):
     """
     Computes team size of each paper by taking the number of authors in 'authors'
     Input:
-        - df['authors']:    dataframe (dataset); or just the 'authors' column               [pandas dataframe]
+        - df:               dataframe (dataset); or just the 'authors' column               [pandas dataframe]
     Output:
-        - Team:             vector of team_size for each paper of the given dataset         [pandas series]
+        - team:             vector of team_size for each paper of the given dataset         [pandas series]
                             with team_size                                                  [int]
     """
-    import pandas as pd
-    
-    Team = pd.Series([len(i) for i in df['authors']])      # Teamsize
+    team = pd.Series([len(i) for i in df['authors']])      # teamsize
 
-    return(Team)                                           # Output
-
-    
+    # Output
+    return(team)
