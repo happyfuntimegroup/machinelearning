481b9fc0bc0e15d72ef7e4347960b32dd51a453d
Albert de Vries <albertwdevries@gmail.com>
2021-12-08 14:51:58 +0100

    Cleaned up

--- CODE/features/length_title.py
+++ CODE/features/length_title.py
@@ -1,22 +1,22 @@
+import pandas as pd
 def length_title(data):
     """
     Computes the length of the title from a paper by counting how many words it contains.
     This function is based on an online published article:
         https://www.natureindex.com/news-blog/five-features-highly-cited-scientific-article
     Input:
-        - df['title']:    dataframe (dataset); 'title' column                           [pandas dataframe]
+        - df['title']:    dataframe (dataset); 'title' column                                   [pandas dataframe]
     Output:
-        - Title_length:   vector of wordlength of for each paper title of the given dataset      [pandas series]
-                            with Title_length                                                   [int]
-    """
-    import pandas as pd
-    
+        - title_length:   vector of wordlength of for each paper title of the given dataset     [pandas series]
+                            with title_length                                                   [int]
+    """    
     out = pd.Series(dtype=pd.Float64Dtype())
 
     for index, i_paper in data.iterrows():
         title = i_paper['title']
-        Title_length = len(title.split())
-        out[index,] = Title_length
+        title_length = len(title.split())
+        out[index,] = title_length          # Add title length to output for each paper
 
+    # Output
     return out
 
