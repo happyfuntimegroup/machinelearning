247c5f787edb149b6bbe026669de17655fcd49d1
Albert de Vries <albertwdevries@gmail.com>
2021-12-08 16:18:59 +0100

    Troubleshooting

--- CODE/features/venue_citations.py
+++ CODE/features/venue_citations.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def venues_citations(data):
+def venues_citations(data, test):
     """Output: 
     {Venue : [list of citations per paper]
     """
@@ -8,7 +8,8 @@ def venues_citations(data):
     citations = []
     venue_venPresL = {}
     
-    out = pd.Series(dtype=pd.Float64Dtype())
+    out_data = pd.Series(dtype=pd.Float64Dtype())
+    out_test = pd.Series(dtype=pd.Float64Dtype())
     
     for index, i_paper in data.iterrows():
         venue = i_paper['venue']
@@ -27,11 +28,19 @@ def venues_citations(data):
         venPresL = (sum_venue / len(value))      
         venue_venPresL[key] = venPresL
     
+    missing_venue = sum(venue_venPresL.values())/len(venue_venPresL.values())
     # For each paper in the dataset, add the venPresL score to that paper; i.e. look at the venue in which it was published,
     #   and add the score to that index in the pandas Series.
     for index, i_paper in data.iterrows():
         venue = i_paper['venue']
-        out[index,] = venue_venPresL[venue]
+        out_data[index,] = venue_venPresL[venue]
+        
+    for index, i_paper in test.iterrows():
+        venue = i_paper['venue']
+        if venue in venue_venPresL.keys():
+            out_test[index,] = venue_venPresL[venue]
+        else:
+            out_test[index,] = missing_venue
     
     # Output
-    return out # Pandas Series of VenPresL for each paper
+    return out_data, out_test # Pandas Series of VenPresL for each paper
